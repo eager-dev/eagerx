50a80fc4edd19eb86dcdb113cd1b99edddc86ef6
Jelle Luijkx <j.d.luijkx@tudelft.nl>
2021-12-02 12:03:38 +0100

    Create installation procedure for OpenDR

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
-from distutils.command.sdist import sdist as sdist_orig
-from distutils.errors import DistutilsExecError
-
+import pathlib
 from setuptools import setup
+from setuptools.command.install import install
+import subprocess
 
 
-class sdist(sdist_orig):
+class CustomInstall(install):
+
+    def __init__(self, dist):
+        super(install, self).__init__(dist)
+        install_path = pathlib.Path(__file__).parent / 'bin' / 'install'
+        subprocess.call(str(install_path.resolve()), shell=True, executable='/bin/bash')
 
     def run(self):
-        try:
-            self.spawn(['ls', '-l'])
-        except DistutilsExecError:
-            self.warn('EAGERx installation failed')
-        super().run()
+        install.run(self)
 
 
 setup(
     name='eagerx',
     version='0.0.1',
-    description='EAGERx: Engine Agnostic Gym Environment with Reactive extension.',
-    url='https://github.com/eager-dev/eagerx',
-    author="Bas van der Heijden and Jelle Luijkx",
-    author_email="D.S.vanderHeijden@tudelft.nl",
-    cmdclass={'sdist': sdist},
+    author='Bas van der Heijden and Jelle Luijkx',
+    author_email='D.S.vanderHeijden@tudelft.nl',
+    packages=[],
+    cmdclass={'install': CustomInstall},
 )
