1c7ae43896bee24bf0b528d7871398901ea6bbac
Jelle Luijkx <j.d.luijkx@tudelft.nl>
2022-02-07 14:33:05 +0100

    Not using jit anymore

--- eagerx/src/eagerx/bridges/ode/ode.py
+++ eagerx/src/eagerx/bridges/ode/ode.py
@@ -1,20 +1,7 @@
-from numba import jit
 from math import sin, exp
 
-@jit(nopython=True)
-def eom_pendulum(x, t, u):
-    J = 0.000189238
-    m = 0.0563641
-    g = 9.81
-    l = 0.0437891
-    b0 = 0.000142205
-    K = 0.0502769
-    R = 9.83536
-    c = 1.49553
-    a = 0.00183742
 
+def eom_pendulum(x, _, u, J=0.000189, m=0.0563, g=9.81, l=0.0438, b0=0.000142, K=0.0503, R=9.84, c=1.50, a=0.00184):
     b = b0 + a * exp(- x[1] * x[1] / (c * c))
-
     ddx = (m * g * l * sin(x[0]) - x[1] * (b + K * K / R) + K * u / R) / J
-
     return [x[1], ddx]
