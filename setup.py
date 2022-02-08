64fe9c373800419bd4e6a42c288700dee4339807
Bas <b.heijden@hotmail.com>
2022-02-08 14:58:55 +0100

    setup eagerx

--- setup.py
+++ setup.py
@@ -1,21 +1,31 @@
-from setuptools import setup
+from distutils.core import setup
 
-packages = ['eagerx'
-            'eagerx.utils'
-            'eagerx.wrappers'
-            'eagerx.gui'
-            'eagerx.core'
+packages = ['eagerx',
+            'eagerx.utils',
+            'eagerx.wrappers',
+            'eagerx.gui',
+            'eagerx.core',
             'eagerx.bridges',
-            'eagerx.nodes'
-            'eagerx.converters'
+            'eagerx.nodes',
+            'eagerx.converters',
             'eagerx.enginestates']
+
 setup(name='eagerx',
       version='0.0',
       description='Engine Agnostic Gym Environments for Robotics',
-      url='https://github.com/eager-dev/eager',
+      url='https://github.com/eager-dev/eagerx',
       author='Bas van der Heijden, Jelle Luijkx',
       author_email='d.s.vanderheijden@tudelft.nl',
       license='MIT',
       packages=packages,
-      package_dir={'main_package': 'src'},
-      zip_safe=False)
+      package_dir={'eagerx': 'src',
+                   'eagerx.utils': 'src/eagerx',
+                   'eagerx.wrappers': 'src/eagerx',
+                   'eagerx.gui': 'src/eagerx',
+                   'eagerx.core': 'src/eagerx',
+                   'eagerx.bridges': 'src/eagerx',
+                   'eagerx.nodes': 'src/eagerx',
+                   'eagerx.converters': 'src/eagerx',
+                   'eagerx.enginestates': 'src/eagerx',
+                   }
+      )
