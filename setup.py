014ab7b789ce24c73226d9153804015c834f091c
Bas <b.heijden@hotmail.com>
2022-02-08 16:13:47 +0100

    setup.py without module import error

--- setup.py
+++ setup.py
@@ -1,31 +1,14 @@
-from distutils.core import setup
+import setuptools
 
-packages = ['eagerx',
-            'eagerx.utils',
-            'eagerx.wrappers',
-            'eagerx.gui',
-            'eagerx.core',
-            'eagerx.bridges',
-            'eagerx.nodes',
-            'eagerx.converters',
-            'eagerx.enginestates']
-
-setup(name='eagerx',
-      version='0.0',
-      description='Engine Agnostic Gym Environments for Robotics',
-      url='https://github.com/eager-dev/eagerx',
-      author='Bas van der Heijden, Jelle Luijkx',
-      author_email='d.s.vanderheijden@tudelft.nl',
-      license='MIT',
-      packages=packages,
-      package_dir={'eagerx': 'src',
-                   'eagerx.utils': 'src/eagerx',
-                   'eagerx.wrappers': 'src/eagerx',
-                   'eagerx.gui': 'src/eagerx',
-                   'eagerx.core': 'src/eagerx',
-                   'eagerx.bridges': 'src/eagerx',
-                   'eagerx.nodes': 'src/eagerx',
-                   'eagerx.converters': 'src/eagerx',
-                   'eagerx.enginestates': 'src/eagerx',
-                   }
-      )
+setuptools.setup(
+    name='eagerx',
+    version='0.0',
+    description='Engine Agnostic Gym Environments for Robotics',
+    url='https://github.com/eager-dev/eagerx',
+    author='Bas van der Heijden, Jelle Luijkx',
+    author_email='d.s.vanderheijden@tudelft.nl',
+    license='Apache2.0',
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
