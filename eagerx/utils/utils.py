9d70ef4d21eb0a0adf944ea7a87188c28ce29181
Bas <b.heijden@hotmail.com>
2022-02-16 18:49:50 +0100

    remove gui utils to eagerx_gui package

--- eagerx/utils/utils.py
+++ eagerx/utils/utils.py
@@ -287,35 +287,6 @@ def _ns(resolved, a, args, context):
             raise
 
 
-def get_ROS_log_level(name):
-    ns = "/".join(name.split("/")[:2])
-    return get_param_with_blocking(ns + "/log_level")
-
-
-def get_yaml_type(yaml):
-    if "node_type" in yaml:
-        if "targets" in yaml:
-            type = "reset_node"
-        else:
-            type = "node"
-    else:
-        type = "object"
-    return type
-
-
-def get_nodes_and_objects_library():
-    from eagerx.core.register import REGISTRY
-
-    library = dict()
-    for entity_cls, entities in REGISTRY.items():
-        library[entity_cls.__name__] = []
-        for id, entry in entities.items():
-            spec = entry["spec"]
-            cls = get_attribute_from_module(entry["cls"])
-            library[entity_cls.__name__].append({"id": id, "spec": spec, "entity_cls": entity_cls, "cls": cls})
-    return library
-
-
 Stamp = NamedTuple("Stamp", [("seq", int), ("sim_stamp", float), ("wc_stamp", float)])
 Stamp.__new__.__defaults__ = (None,) * len(Stamp._fields)
 Info = NamedTuple(
